c767ad3a0307c922ba31269b8f032577cc9f2784
Jaakko Eränen <jaakko.eranen@astehelsinki.fi>
2020-11-24 19:02:05 +0200

    add fake aws access and secret keys so we can use this without aws cli conficurations

--- src/dynamodb.factory.js
+++ src/dynamodb.factory.js
@@ -1,9 +1,11 @@
 const { DocumentClient } = require('aws-sdk/clients/dynamodb');
 
-const withProcessEnv = ({ AWS_ENDPOINT, AWS_REGION }) => () => {
+const withProcessEnv = ({ AWS_ENDPOINT, AWS_REGION, AWS_ACCESS_KEY, AWS_SECRET_KEY }) => () => {
   const options = {
     endpoint: AWS_ENDPOINT,
-    region: AWS_REGION
+    region: AWS_REGION,
+    accessKeyId: AWS_ACCESS_KEY,
+    secretAccessKey: AWS_SECRET_KEY
   };
 
   return new DocumentClient(options);
